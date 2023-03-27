16a272d789dbd3cab6309240ce413af647236c7d
Steven Adetomiwa <s.adetomiwa@icloud.com>
2023-03-27 09:30:38 -0500

    fix

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ from flask_login import login_user, logout_user, login_required, current_user
 
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     addresses = Address.query.all()
     form = SearchForm()
